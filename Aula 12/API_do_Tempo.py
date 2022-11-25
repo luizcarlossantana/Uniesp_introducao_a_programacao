c754fa30c1ff92ea847d201d3a6261d8be75b91d
luizcarlossantana <2022211510009@iesp.edu.br>
2022-11-25 17:20:06 -0300

    ....

--- Aula 12/API_do_Tempo.py
+++ Aula 12/API_do_Tempo.py
@@ -1,16 +1,32 @@
-import requests
-import json
+
 
 API_KEY = '639d50780031bb94a42a33ddf2cee2d2'
-LAT = -7.11532
-LOG = -34.861
-url = (f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LOG}&appid={API_KEY}")
 
-resposta   = requests.request("GET", url)
-objetos    = json.loads(resposta.text)
-# dados      = objetos['dados']
+from requests import get 
+from json import dumps
 
-for i in objetos:
-    print(f"{i} :: {objetos[i]}")
+API_KEY = input("Digite sua chave para utilizar a API: ")
+arquivo = input("Informe o arquivo com os dados que deve ser consultados: ")
 
-# https://api.openweathermap.org/data/2.5/weather?lat={-7.11532}&lon={-34.861}&appid={2f2f555888d23e5a36c5aad583ebfae9}
+
+dados = []
+with open(arquivo , 'r', encoding='UTF-8') as x:
+    for i in x.readlines():
+        print(i)
+        i = i.replace('\n','').replace(' ','') 
+        formato1 = i.split(',') 
+        dados.append(formato1) 
+
+for informaçao in dados:    
+   
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={informaçao[3]}&lon={informaçao[2]}&appid={API_KEY}'   
+    resposta = get(url)
+    print(resposta.text)
+
+    if resposta.status_code == 200:
+        print(f'Criando arquivo: {informaçao[0]}.json')
+        with open(informaçao[0] + '.json', 'w', encoding='UTF-8') as arquivo:
+            dadosjson = dumps({informaçao[1]:resposta.json()}, indent=4) 
+            arquivo.write(dadosjson)
+    else:
+        print(f'Ocorreu um erro ao consultar as informações para: {informaçao}')
